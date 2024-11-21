7f7035defd4a77213da66e16371218563fabf156
AaronDeluna <aaron_deluna@mail.ru>
2024-11-21 18:36:09 +0300

    Рефакторинг: создан абстрактный класс AbstractInsuranceCalcService и вынесена реализация метода calculateInsuranceCost для соблюдения принципа DRY

--- src/main/java/org/javaacademy/insurance/service/brazil/InsuranceCalcBrazilService.java
+++ src/main/java/org/javaacademy/insurance/service/brazil/InsuranceCalcBrazilService.java
@@ -1,40 +1,15 @@
 package org.javaacademy.insurance.service.brazil;
 
-import lombok.RequiredArgsConstructor;
 import org.javaacademy.insurance.config.InsuranceCalculationProperties;
-import org.javaacademy.insurance.model.InsuranceType;
-import org.javaacademy.insurance.service.InsuranceCalcService;
-import org.javaacademy.insurance.utils.CalculateCostUtils;
+import org.javaacademy.insurance.service.AbstractInsuranceCalcService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
-
-import static java.math.BigDecimal.ZERO;
-
 @Component
 @Profile("brazil")
-@RequiredArgsConstructor
-public class InsuranceCalcBrazilService implements InsuranceCalcService {
-    private final InsuranceCalculationProperties properties;
-    private BigDecimal price = ZERO;
+public class InsuranceCalcBrazilService extends AbstractInsuranceCalcService {
 
-    /**
-     * Расчет стоимости страховки в Японии.
-     *
-     * @param insuredAmount сумма покрытия.
-     * @param insuranceType тип старховки.
-     * @return стоимоть страховки.
-     */
-    @Override
-    public BigDecimal calculateInsuranceCost(BigDecimal insuredAmount, InsuranceType insuranceType) {
-        switch (insuranceType) {
-            case THEFT -> price = CalculateCostUtils.calculateCost(insuredAmount,
-                    properties.getRobberyCoefficient(), properties.getTheftServiceCost());
-            case MEDICAL -> price = CalculateCostUtils.calculateCost(insuredAmount,
-                    properties.getMedicalCaseCoefficient(), properties.getMedicalServiceCost());
-            default -> throw new IllegalArgumentException();
-        }
-        return price;
+    public InsuranceCalcBrazilService(InsuranceCalculationProperties properties) {
+        super(properties);
     }
 }
--- src/main/java/org/javaacademy/insurance/service/japan/InsuranceCalcJapanService.java
+++ src/main/java/org/javaacademy/insurance/service/japan/InsuranceCalcJapanService.java
@@ -1,40 +1,14 @@
 package org.javaacademy.insurance.service.japan;
 
-import lombok.RequiredArgsConstructor;
 import org.javaacademy.insurance.config.InsuranceCalculationProperties;
-import org.javaacademy.insurance.model.InsuranceType;
-import org.javaacademy.insurance.service.InsuranceCalcService;
-import org.javaacademy.insurance.utils.CalculateCostUtils;
+import org.javaacademy.insurance.service.AbstractInsuranceCalcService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
-
-import static java.math.BigDecimal.ZERO;
-
 @Component
 @Profile("japan")
-@RequiredArgsConstructor
-public class InsuranceCalcJapanService implements InsuranceCalcService {
-    private final InsuranceCalculationProperties properties;
-    private BigDecimal price = ZERO;
-
-    /**
-     * Расчет стоимости страховки в Японии.
-     *
-     * @param insuredAmount сумма покрытия.
-     * @param insuranceType тип старховки.
-     * @return стоимоть страховки.
-     */
-    @Override
-    public BigDecimal calculateInsuranceCost(BigDecimal insuredAmount, InsuranceType insuranceType) {
-        switch (insuranceType) {
-            case THEFT -> price = CalculateCostUtils.calculateCost(insuredAmount,
-                    properties.getRobberyCoefficient(), properties.getTheftServiceCost());
-            case MEDICAL -> price = CalculateCostUtils.calculateCost(insuredAmount,
-                    properties.getMedicalCaseCoefficient(), properties.getMedicalServiceCost());
-            default -> throw new IllegalArgumentException();
-        }
-        return price;
+public class InsuranceCalcJapanService extends AbstractInsuranceCalcService {
+    public InsuranceCalcJapanService(InsuranceCalculationProperties properties) {
+        super(properties);
     }
 }
